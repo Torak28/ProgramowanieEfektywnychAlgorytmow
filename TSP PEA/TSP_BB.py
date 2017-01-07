34f7ea1efeb52c3070a2226fa2fb64ae64d4c89d
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-01-07 23:58:31 +0100

    fix typo

--- TSP PEA/TSP_BB.py
+++ TSP PEA/TSP_BB.py
@@ -13,8 +13,8 @@ def dlugoscDrogi(tablica, macierz):
     sum = 0
     pary = generujPary(tablica)
     for i in range(pary.__len__()):
-        miastoAIndex = pary[i][1] - 1
-        miastoBIndex = pary[i][0] - 1
+        miastoAIndex = pary[i][0] - 1
+        miastoBIndex = pary[ i ][ 1 ] - 1
         sum = sum + macierz[miastoAIndex][miastoBIndex]
     return sum
 
