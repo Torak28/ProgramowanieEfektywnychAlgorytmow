52e65b553b493c1dc6ee0691faac41c308399321
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-01-07 19:36:30 +0100

    Dodanie minimum

--- TSP PEA/TSP_DP.py
+++ TSP PEA/TSP_DP.py
@@ -11,12 +11,32 @@ def dp(macierz):
     dane = []
     for i in range(Ilosc_Miast):
         dane.append(i+1)
-    print len(dane)
+    print len(dane) #rowne Ilosc_Miast
+    
+def minimum(celDrogi ,sciezkaPrzejscia):
+    if (celDrogi ,sciezkaPrzejscia) in krotkiPrzejscia:
+        return krotkiPrzejscia[celDrogi ,sciezkaPrzejscia]
+    wartosci = [ ]
+    minimalnePrzejscie = [ ]
+    for j in sciezkaPrzejscia:
+        przejscie = copy.deepcopy(list(sciezkaPrzejscia))
+        przejscie.remove(j)
+        minimalnePrzejscie.append([ j, tuple(przejscie) ])
+        wynik = minimum(j, tuple(przejscie))
+        wartosci.append(macierz[ celDrogi - 1 ][ j - 1 ] + wynik)
 
+    # get minimun value from set as optimal solution for
+    krotkiPrzejscia[ celDrogi, sciezkaPrzejscia ] = min(wartosci)
+    permutacje.append(((celDrogi, sciezkaPrzejscia), minimalnePrzejscie[ wartosci.index(krotkiPrzejscia[ celDrogi, sciezkaPrzejscia ]) ]))
+
+    return krotkiPrzejscia[ celDrogi, sciezkaPrzejscia ]
 # Obsluga menu
 wybor = int(input('Wybieramy!\n\t1.Wpisuje z palca(do dopisania obsluga)\n\t2.Wczytam z pliku\nHmm?\n'))
 if wybor == 1:
     tabWpisywanie = []
+    wszystkieSubsety = [ ]
+    krotkiPrzejscia = {}
+    permutacje = [ ]
     Ilosc_Miast = int(input('Ilosc Miast = '))
     macierz = [[0 for i in range(Ilosc_Miast)] for j in range(Ilosc_Miast)]
     print('Koszty przejazdu? (Liczba i enter. Sam program rozkmini jak to leci)')
@@ -32,6 +52,9 @@ if wybor == 1:
         tabWpisywanie.append(i)
 elif wybor == 2:
     tabPlik = []
+    wszystkieSubsety = []
+    krotkiPrzejscia = {}
+    permutacje = []
     nazwa = raw_input('Jak sie nazywa pliczek? ')
     plik = open(nazwa)
     # Podzial pliku na liczby
