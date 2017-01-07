e55f11cb604374a53fd54df2c40a314b3acb3a4b
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-01-07 18:06:51 +0100

    init do Dynamicznego programowania

---
+++ TSP PEA/TSP_DP.py
@@ -0,0 +1,68 @@
+# Autor : Jarek Ciolek-Zelechowski
+
+import copy
+import itertools
+import time
+
+INF = -1
+ile = 0
+
+# Obsluga menu
+wybor = int(input('Wybieramy!\n\t1.Wpisuje z palca(do dopisania obsluga)\n\t2.Wczytam z pliku\nHmm?\n'))
+if wybor == 1:
+    tabWpisywanie = []
+    Ilosc_Miast = int(input('Ilosc Miast = '))
+    macierz = [[0 for i in range(Ilosc_Miast)] for j in range(Ilosc_Miast)]
+    print('Koszty przejazdu? (Liczba i enter. Sam program rozkmini jak to leci)')
+    for i in range(Ilosc_Miast):
+        for j in range(Ilosc_Miast):
+            macierz[i][j] = int(input())
+
+    rozmiar = len(macierz)
+    for i in range(rozmiar):
+        macierz[i][i] = INF
+
+    for i in range(Ilosc_Miast):
+        tabWpisywanie.append(i)
+elif wybor == 2:
+    tabPlik = []
+    nazwa = raw_input('Jak sie nazywa pliczek? ')
+    plik = open(nazwa)
+    # Podzial pliku na liczby
+    tab = [word for line in plik for word in line.split()]
+
+    Ilosc_Miast = int(tab[0])
+    macierz = [[0 for i in range(Ilosc_Miast)] for j in range(Ilosc_Miast)]
+
+    # Usuniecie poczatku mowiacego o Ilosci miast
+    tab.remove(tab[0])
+
+    # Przepisanie tablicy liczb do macierzy
+    pom = 0
+    for i in range(Ilosc_Miast):
+       for j in range(Ilosc_Miast):
+            macierz[i][j] = tab[pom]
+            pom += 1
+
+    macierz = [ map(int, x) for x in macierz ]
+
+    rozmiar = len(macierz)
+    for i in range(rozmiar):
+        macierz[i][i] = INF
+    for i in range(Ilosc_Miast):
+        tabPlik.append(i)
+
+# Drukiwanie zadanej na poczatku macierzy
+print('Wypisanie:')
+for i in range(rozmiar):
+    print(macierz[ i ])
+
+# Wypisanie wyniku
+print "\n"
+start = time.clock()
+# droga,dyst = dp(macierz)
+end = time.clock()
+total = end - start
+print "Najkrotsza droga: ", #droga
+print "Jej dlugosc: ", #dyst
+print("Czas pomiaru: {0:02f}s".format(total))
