adb5edd0ba1a962b07c3b41910d355ee223a9e31
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-01-22 00:08:41 +0100

    getting there. Dodanie wybierania wierzcholka

--- TSP PEA/TSP_AP.py
+++ TSP PEA/TSP_AP.py
@@ -10,11 +10,48 @@ import time
 INF = 0
 ile = 0
 
+# Funkcja dla zadanej tablicy drogi generuje pary
+# Np. dla drogi = [1,2,3]
+# Wygeneruje wynik = [[1,2],[2,3]]
+# Potrzebne przy obliczaniu Dlugosci Drogi
+def generujPary(tablica):
+    wynik = []
+    for i in range(tablica.__len__()-1):
+        wynik.extend([ [ tablica[ i ], tablica[ i+1 ] ] ])
+    return wynik
+
+# Dla przyjetej tablicy drogi i zadanej macierzy przejscia
+# wylicza i zwraca dlugosc drogi
+def dlugoscDrogi(tablica, macierz):
+    sum = 0
+    pary = generujPary(tablica)
+    for i in range(pary.__len__()):
+        miastoAIndex = pary[i][0] - 1
+        miastoBIndex = pary[ i ][ 1 ] - 1
+        sum = sum + macierz[miastoAIndex][miastoBIndex]
+    return sum
+
+def wybierzWiercholek(dist):
+    return dist.index(max(dist)) + 1
+
 def ap(macierz):
+    dist = []
+    koszt = 0
+    droga = [1,1]
+    for i in range(Ilosc_Miast):
+        dist.append(macierz[0][i])
+
+    """
+    Teoretyczna petla for
+    """
+    print wybierzWiercholek(dist)
+
+
+    print dist
     return 1,2
 
 # Obsluga menu
-wybor = int(input('Wybieramy!\n\t1.Wpisuje z palca(do dopisania obsluga)\n\t2.Wczytam z pliku\nHmm?\n'))
+wybor = 2#int(input('Wybieramy!\n\t1.Wpisuje z palca(do dopisania obsluga)\n\t2.Wczytam z pliku\nHmm?\n'))
 if wybor == 1:
     tabWpisywanie = []
     wszystkieSubsety = [ ]
@@ -38,7 +75,7 @@ elif wybor == 2:
     wszystkieSubsety = []
     krotkiPrzejscia = {}
     permutacje = []
-    nazwa = raw_input('Jak sie nazywa pliczek? ')
+    nazwa = "test"#raw_input('Jak sie nazywa pliczek? ')
     plik = open(nazwa)
     # Podzial pliku na liczby
     tab = [word for line in plik for word in line.split()]
