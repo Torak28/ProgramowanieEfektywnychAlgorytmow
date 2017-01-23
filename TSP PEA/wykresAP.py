13f78fdf57743eba0df89b44b7d82e1a488163b4
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-01-23 09:37:39 +0100

    Wykresy 2

--- TSP PEA/wykresAP.py
+++ TSP PEA/wykresAP.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 plt.plot([1,2,3,4,5,6,7], [0.000101, 0.000247, 0.001493, 0.001784, 0.002311, 0.002642, 0.003513], 'bo')
 plt.plot([1,2,3,4,5,6,7], [0.000101, 0.000247, 0.001493, 0.001784, 0.002311, 0.002642, 0.003513])
 
-plt.xlabel('Kolejne Pliki z Miastami[4,6,12,13,14,15,17]')
+# plt.xlabel('Kolejne Pliki z Miastami[4,6,12,13,14,15,17]')
+plt.xlabel('Ilosc Miast')
 plt.ylabel('Czas')
-plt.title('Wykres czasu dla kolejnych plikow z miastami BB')
-plt.axis([0, 8, 0, 0.005])
+plt.title('Wykres czasu dla kolejnych plikow z miastami AP')
+plt.axis([0, 8, 0, 0.004])
 plt.grid(True)
 plt.show()
